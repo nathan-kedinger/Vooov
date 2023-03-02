c98f967c6345b05db0efd318d19ae3577d0ad46f
nathan-kedinger <51140084+nathan-kedinger@users.noreply.github.com>
2023-03-02 20:34:53 +0100

    Improve studio activity, chronometer is now working during recording and record reading

--- app/src/main/java/com/example/vooov/StudioActivity.kt
+++ app/src/main/java/com/example/vooov/StudioActivity.kt
@@ -9,9 +9,12 @@ import android.media.MediaRecorder
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.os.SystemClock
 import android.util.Log
 import android.widget.ImageButton
+import android.widget.SeekBar
+import android.widget.SeekBar.OnSeekBarChangeListener
 import androidx.core.app.ActivityCompat
 import com.example.vooov.data.model.RecordModel
 import com.example.vooov.databinding.ActivityStudioBinding
@@ -20,7 +23,9 @@ import com.example.vooov.viewModels.CurrentUser
 import com.example.vooov.viewModels.RecordsViewModel
 import kotlinx.coroutines.*
 import java.io.IOException
+import java.text.SimpleDateFormat
 import java.util.*
+import java.util.concurrent.TimeUnit
 
 private const val LOG_TAG = "AudioRecordTest"
 private const val REQUEST_RECORD_AUDIO_PERMISSION = 200
@@ -161,41 +166,48 @@ class StudioActivity : AppCompatActivity() {
     private fun onPlay(start: Boolean) = if (start) {
         startPlaying()
     } else {
-        stopPlaying()
+        pausePlaying()
     }
+
     private fun startPlaying() {
         player = MediaPlayer().apply {
             try {
                 setDataSource(fileFullPath)
                 prepare()
+                startTimer()
                 start()
-                    startMeterProgressTime()
-                    CoroutineScope(Dispatchers.Main).launch {
-                        stopMeterProgressTimeWhenFinish()
-                    }
-
+                startTimeCounting()
+                startMeterProgressTime()
+                CoroutineScope(Dispatchers.Main).launch {
+                    stopMeterProgressTimeWhenFinish()
+                }
             } catch (e: IOException) {
                 Log.e(LOG_TAG, "prepare() failed")
             }
         }
     }
 
-    private fun pausePlaing(){
+    private fun pausePlaying() {
         player?.pause()
-    }
-    private fun stopPlaying() {
+        stopTimeCounting()
         stopMeterProgressTime()
-        player?.release()
-        player = null
+        stopTimer()
     }
 
+    private fun stopPlaying() {
+        stopMeterTotalTime()
+        player?.release()
+        player = null
+        stopMeterProgressTime()
+        stopTimer()
+        binding.studioSeekBar.progress = 0
+    }
 
     private fun startTimeCounting() {
         startTime = System.currentTimeMillis()
-        startMeterTotalTime()
     }
 
-    private fun stopTimeCounting(){
+    private fun stopTimeCounting() {
         stopTime = System.currentTimeMillis()
     }
 
@@ -203,47 +215,97 @@ class StudioActivity : AppCompatActivity() {
         return stopTime - startTime
     }
 
-    private fun startMeterTotalTime(){
+    private fun startMeterTotalTime() {
         binding.studioMeterTotalTime.start()
     }
 
-    private fun stopMeterTotalTime(){
+    private fun stopMeterTotalTime() {
         binding.studioMeterTotalTime.stop()
     }
 
-    private fun reinitializeMeterTotalTime(){
-        binding.studioMeterTotalTime.base= SystemClock.elapsedRealtime()
+    private fun reinitializeMeterTotalTime() {
+        binding.studioMeterTotalTime.base = SystemClock.elapsedRealtime()
     }
 
     private var elapsedTime: Long = 0
     private var isRunning: Boolean = false
-    private fun startMeterProgressTime(){
-        if(!isRunning){
+
+    private fun startMeterProgressTime() {
+        if (!isRunning) {
             binding.studioMeterProgressingTime.base = SystemClock.elapsedRealtime() - elapsedTime
             binding.studioMeterProgressingTime.start()
             isRunning = true
         }
     }
-    private fun stopMeterProgressTime(){
-        if(isRunning) {
+
+    private fun stopMeterProgressTime() {
+        if (isRunning) {
             elapsedTime = SystemClock.elapsedRealtime() - binding.studioMeterProgressingTime.base
             binding.studioMeterProgressingTime.stop()
             isRunning = false
         }
     }
+
     private fun reinitializeMeterProgressTime() {
         binding.studioMeterProgressingTime.base = SystemClock.elapsedRealtime()
-        stopMeterProgressTime()
         elapsedTime = 0
-    }
-    suspend private fun stopMeterProgressTimeWhenFinish(){
-        while (binding.studioMeterProgressingTime.text != binding.studioMeterTotalTime.text) {
-            delay(100)
-        }
+        binding.studioSeekBar.progress = 0
         onPlay(start = false)
         binding.studioPlayButton.setBackgroundResource(R.drawable.ic_play)
     }
 
+    private fun stopMeterProgressTimeWhenFinish() {
+        player?.setOnCompletionListener {
+            stopMeterProgressTime()
+            stopTimer()
+            binding.studioSeekBar.progress = binding.studioSeekBar.max
+            binding.studioPlayButton.setBackgroundResource(R.drawable.ic_play)
+        }
+    }
+    private var timer = Timer()
+
+    private fun startTimer() {
+        timer.cancel()
+        val timer = Timer()
+        startTime = 0 //System.currentTimeMillis()
+        timer.schedule(object : TimerTask() {
+            override fun run() {
+                val progress = player?.let {
+                    if (it.duration > 0) {
+                        it.currentPosition / (it.duration / 100)
+                    } else {
+                        0
+                    }
+                } ?: 0
+                binding.studioSeekBar.progress = progress
+            }
+        }, 100, 100)
+
+        binding.studioSeekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                if (fromUser) {
+                    player?.seekTo(progress * 100)
+
+                    val duration = player?.duration ?: 0
+                    val time = duration * progress / 100
+                    binding.studioMeterProgressingTime.text = formatTime(time.toLong())
+                }
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+        })
+    }
+    private fun stopTimer() {
+        timer.cancel()
+        binding.studioSeekBar.progress = 1
+    }
+    private fun formatTime(millis: Long): String {
+        val minutes = TimeUnit.MILLISECONDS.toMinutes(millis)
+        val seconds = TimeUnit.MILLISECONDS.toSeconds(millis) - TimeUnit.MINUTES.toSeconds(minutes)
+        return String.format("%02d:%02d", minutes, seconds)
+    }
     private fun startRecording() {
         /* appui sur le bouton enregistrement : remise à zéro du compteur
                                                 départ  des deux chronos
@@ -265,8 +327,8 @@ class StudioActivity : AppCompatActivity() {
                 setAudioEncoder(MediaRecorder.AudioEncoder.HE_AAC)
                 startTimeCounting()
                 reinitializeMeterTotalTime()
+                reinitializeMeterProgressTime()
                 startMeterTotalTime()
-
                 try {
                     prepare()
                 } catch (e: IOException) {
@@ -283,6 +345,7 @@ class StudioActivity : AppCompatActivity() {
                 setAudioEncoder(MediaRecorder.AudioEncoder.HE_AAC)
                 startTimeCounting()
                 reinitializeMeterTotalTime()
+                reinitializeMeterProgressTime()
                 startMeterTotalTime()
 
                 try {
@@ -323,15 +386,3 @@ class StudioActivity : AppCompatActivity() {
         binding.studioMeterProgressingTime.stop()
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
